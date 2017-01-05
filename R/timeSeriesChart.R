b9525f2843f84aba324c8efc2ce27d2a55f2a533
Santiago Allende <santi.allende@me.com>
2017-01-05 09:09:58 -0800

    added time series chart function and updated packages in desc.

---
+++ R/timeSeriesChart.R
@@ -0,0 +1,37 @@
+#' Time Series Chart
+#'
+#' Creates a time series chart with data structured in long format. Dependenceis:
+#' ggplot2 and plotly.
+#'
+#' @param data Is the dataframe that you are interested in visualizing.
+#'
+#' @param timepoint Is the name of the column containing ordered time points.
+#'
+#' @param response Is the name of the column containing the dependent (y axis) variable values.
+#'
+#' @param tx Is optional and may be specified to create an interaction graph. It is the name
+#' of the column containing treatment group membership.
+#'
+#' @examples timeSeriesChart(meanMcs, time, value, MBSR1CBT2)
+#'
+#' @export
+
+timeSeriesChart <- function(data, timepoint, response, tx) {
+        if (missing(tx)) {
+                line <- ggplot(data, aes_string(timepoint, response, group = 1))
+                line + stat_summary(fun.y = mean, geom = "point", colour = "skyblue3") +
+                        stat_summary(fun.y = mean, geom = "line", colour = "skyblue3") +
+                        stat_summary(fun.data = mean_cl_boot, geom = "errorbar",
+                                     width = 0.5, colour = "skyblue3") +
+                        labs(x = "timepoint", y = "response", title = substitute(data))
+                ggplotly()
+        } else {
+                line <- ggplot(data, aes_string(timepoint, response, group = 1, colour = tx))
+                line + stat_summary(fun.y = mean, geom = "point", aes_string(group = tx)) +
+                        stat_summary(fun.y = mean, geom = "line", aes_string(group = tx)) +
+                        stat_summary(fun.data = mean_cl_boot, geom = "errorbar",
+                                     aes_string(group = tx), width = 0.2) +
+                        labs(x = "timepoint", y = "response", colour = "tx", title = substitute(data))
+                ggplotly()
+        }
+}
