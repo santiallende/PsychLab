3df868ca511200b99307373074cc1dc4d78d4d13
Santiago Allende <santi.allende@me.com>
2017-01-25 11:47:14 -0800

    color err

--- R/timeSeriesChart.R
+++ R/timeSeriesChart.R
@@ -28,7 +28,7 @@ timeSeriesChart <- function(data, timepoint, response, tx) {
                 line <- ggplot(data, aes_string(timepoint, response, group = 1, colour = tx))
                 line + stat_summary(fun.y = mean, geom = "point", aes_string(group = tx)) +
                         stat_summary(fun.y = mean, geom = "line", aes_string(group = tx)) +
-                        stat_summary(fun.data = mean_cl_normal, geom = "errorbar",
+                        stat_summary(fun.data = mean_cl_normal, geom = "errorbar", aes_string(group = tx),
                                      position = position_dodge(width = 0.90), width = 0.2) +
                         labs(x = "timepoint", y = "response", colour = "tx", title = substitute(data))
         }
